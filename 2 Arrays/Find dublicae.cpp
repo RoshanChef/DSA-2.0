7e7e5e91680318fb8e0bf3efec722143287a4b66
RoshanChef <kalmatheroshan@gmail.com>
2024-06-01 11:54:49 +0530

    changes

--- 2 Arrays/Find dublicae.cpp
+++ 2 Arrays/Find dublicae.cpp
@@ -1,34 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std; 
-     vector<int> duplicates(long long arr[], int n) {
-        sort(arr, arr + n);
-        vector<int> ans;
-        int track=-1;
-        
-        for(int i = 0; i<n-1; i++){
-            
-            if(arr[i]==arr[i+1] && track !=arr[i] ){
-                ans.push_back(arr[i+1]);
-                track = arr[i];
-            }
-        }
-   
-        if(ans.size()>0 ){
-            return ans;
-        }
-    
-        ans.push_back(-1);
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> duplicates(long long arr[], int n)
+{
+    sort(arr, arr + n);
+    vector<int> ans;
+    int track = -1;
 
-        return ans;
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        if (arr[i] == arr[i + 1] && track != arr[i])
+        {
+            ans.push_back(arr[i + 1]);
+            track = arr[i];
+        }
     }
 
-int main(){
-    
-    long long arr[]= {1,4,2,3,4,1};
-    
-    vector<int> ans = duplicates(arr , 6); 
+    if (ans.size() > 0)
+    {
+        return ans;
+    }
+    return {-1};
+}
 
-    for(int val:ans){   cout<<val<<" ";  }
+int main()
+{
 
-return 0;
+    long long arr[] = {1, 41, 2, 3, 41, 1, 41};
+
+    vector<int> ans = duplicates(arr, 6);
+
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
 }
