ef2b97101ce8271325a2b16cc9b2a43f1baa101a
RoshanChef <kalmatheroshan@gmail.com>
2024-06-04 17:30:01 +0530

    change

---
+++ 6 LinkList/A singly/11.Random.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// online practice
+//  Brute O(N*N)
+//  map : TC  O(N)
+//          : SC O(N)
+//  efficient O(N)
+
+int main()
+{
+
+    return 0;
+}
