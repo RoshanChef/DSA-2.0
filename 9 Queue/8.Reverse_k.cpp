3a72b800e3a94ab5c95f8cb63810d902b82eaf94
RoshanChef <kalmatheroshan@gmail.com>
2024-06-05 00:50:01 +0530

    change

---
+++ 9 Queue/8.Reverse_k.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(queue<int> &q, int k)
+{
+    if (q.empty() || k == 0)
+        return;
+
+    int val = q.front();
+    q.pop();
+
+    reverse_k(q, k - 1);
+
+    q.push(val);
+}
+void reverse_k(queue<int> &q, int k)
+{
+    solve(q, k);
+    int len = q.size() - len; 
+    
+    
+}
+
+void print(queue<int> &q)
+{
+    if (q.empty())
+        return;
+    int len = q.size();
+    while (len)
+    {
+        int val = q.front();
+        cout << val << " ";
+        q.pop();
+        q.push(val);
+        len--;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    print(q);
+
+    reverse_k(q, 3);
+
+    print(q);
+
+    return 0;
+}
