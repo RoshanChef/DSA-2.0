3511e1f3b2f2763aaa18b25f9690f24dd3fbe95a
RoshanChef <kalmatheroshan@gmail.com>
2024-06-07 13:19:14 +0530

    change

---
+++ 8 Stack/13.getMin_stack.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MinStack
+{
+    int minEle;
+    stack<int> s;
+
+public:
+    int getMin()
+    {
+        if (s.empty())
+            return -1;
+        return minEle;
+    }
+
+    int pop()
+    {
+        if (s.empty())
+            return -1;
+
+        if (s.top() >= minEle)
+        {
+            int ans = s.top();
+            s.pop();
+            return ans;
+        }
+        else
+        {
+            int ans = minEle;
+            minEle = 2 * minEle - s.top();
+            s.pop();
+            return ans;
+        }
+    }
+
+    void push(int x)
+    {
+        if (s.empty())
+        {
+            s.push(x);
+            minEle = x;
+        }
+        else
+        {
+            if (x >= minEle)
+            {
+                s.push(x);
+            }
+            else
+            {
+                // x<minEle
+                s.push(2 * x - minEle);
+                minEle = x;
+            }
+        }
+    }
+};
