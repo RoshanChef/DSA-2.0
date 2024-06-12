f09220fde27663548b41e2790b96be8e02cb4e18
RoshanChef <kalmatheroshan@gmail.com>
2024-06-12 21:50:35 +0530

    'chnage'

--- 10 Binary Tree/Questions/13.Boundry view.cpp
+++ 10 Binary Tree/Questions/13.Boundry view.cpp
@@ -4,13 +4,13 @@ class node
 {
 
 public:
-    int val;
+    int data;
     node *right;
     node *left;
 
     node(int val)
     {
-        this->val = val;
+        this->data = data;
         this->right = 0;
         this->left = 0;
     }
@@ -29,14 +29,14 @@ void levelBuild(node *&root)
         node *temp = que.front();
         que.pop();
 
-        cout << "enter data for left of " << temp->val << endl;
+        cout << "enter data for left of " << temp->data << endl;
         cin >> data;
         if (data != -1)
         {
             temp->left = new node(data);
             que.push(temp->left);
         }
-        cout << "enter the data for right of " << temp->val << endl;
+        cout << "enter the data for right of " << temp->data << endl;
         cin >> data;
         if (data != -1)
         {
@@ -44,6 +44,72 @@ void levelBuild(node *&root)
             que.push(temp->right);
         }
     }
+}
+
+void traverseLeft(node *root, vector<int> &ans)
+{
+    if ((root == 0) || (root->left == 0 && root->right == 0))
+        return;
+
+    // simple
+    ans.push_back(root->data);
+
+    if (root->left)
+        traverseLeft(root->left, ans);
+    else
+        traverseLeft(root->right, ans);
+}
+
+void traverseLeaf(node *root, vector<int> &ans)
+{
+    if (root == 0)
+        return;
+    if (root->left == 0 && root->right == 0)
+    {
+        ans.push_back(root->data);
+        return;
+    }
+    traverseLeaf(root->left, ans);
+    traverseLeaf(root->right, ans);
+}
+
+void traverseRight(node *root, vector<int> &ans)
+{
+    if ((root == 0) || (root->left == 0 && root->right == 0))
+        return;
+
+    if (root->right)
+        traverseRight(root->right, ans);
+    else
+        traverseRight(root->left, ans);
+    // reverse
+    ans.push_back(root->data);
+}
+
+vector<int> boundary(node *root)
+{
+    if (root == 0)
+        return {};
+    if (root->left == 0 && root->right == 0)
+        return {root->data};
+
+    // add root
+    vector<int> ans;
+    ans.push_back(root->data);
+
+    // left part exclusive leaf
+    traverseLeft(root->left, ans);
+
+    // leaf of left subtree
+    traverseLeaf(root->left, ans);
+
+    // leaf of right subtree
+    traverseLeaf(root->right, ans);
+
+    // righ part exclusive leaf
+    traverseRight(root->right, ans);
+
+    return ans;
 }
 
 int main()
