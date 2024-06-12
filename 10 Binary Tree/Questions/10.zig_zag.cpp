cf42dd811d7081726b44985b3b1fafb581287200
Roshan Kalmathe <103868228+RoshanChef@users.noreply.github.com>
2024-06-12 17:07:14 +0530

    Update 10.zig_zag.cpp

--- 10 Binary Tree/Questions/10.zig_zag.cpp
+++ 10 Binary Tree/Questions/10.zig_zag.cpp
@@ -1,9 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-int main()
-{
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int>> res;
+    if (root == 0)
+        return {};
+    if (root->left == 0 && root->right == 0)
+        return {{root->val}};
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    bool lTor = 1;
+
+    while (!q.empty()) {
+
+        int size = q.size();
+        vector<int> ans_t(size);
+
+        // Process Queue
+        for (int i = 0; i < size; i++) {
+
+            TreeNode* front = q.front();
+            q.pop();
+            // reverse or normal
+            int index = lTor ? i : size - 1 - i;
+            ans_t[index] = front->val;
+
+            if (front->left)
+                q.push(front->left);
+            if (front->right)
+                q.push(front->right);
+        }
+
+        // change direction
+        lTor = !lTor;
+
+        res.push_back(ans_t);
+    }
+    return res;
+}
+
+int main() { 
     
- return 0;
+    
+    return 0;
 }
