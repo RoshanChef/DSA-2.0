7f1d0c17fd2c4ee18fe32cab6351aeb4e1e54fb1
RoshanChef <kalmatheroshan@gmail.com>
2024-06-14 18:54:07 +0530

    'change'

---
+++ 11.BST/2.SearchBST.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *right;
+    node *left;
+
+    node(int data)
+    {
+        this->data = data;
+        this->right = 0;
+        this->left = 0;
+    }
+};
+
+// Traversal
+void level_order(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            // level complete
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+node *insertionBST(node *root, int d)
+{
+    if (root == 0)
+    {
+        root = new node(d);
+        return root;
+    }
+
+    if (d > root->data)
+    {
+        // right side
+        root->right = insertionBST(root->right, d);
+    }
+    else
+    {
+        // left side
+        root->left = insertionBST(root->left, d);
+    }
+    return root;
+}
+// Time complexity:  O( logN )  || WC : O(N) if node des
+// Space complexity: O( logN )
+
+void takeInput(node *&root)
+{
+    int data;
+    cin >> data;
+    while (data != -1)
+    {
+        root = insertionBST(root, data);
+        cin >> data;
+    }
+}
+
+int main()
+{
+
+    node *root = 0;
+
+    cout << "Enter the data " << endl;
+    takeInput(root);
+    cout << "Data is " << endl;
+    level_order(root);
+
+    return 0;
+}
