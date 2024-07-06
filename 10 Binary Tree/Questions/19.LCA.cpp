ade7166f57718119d65f5cdc41eef2c1339eca27
RoshanChef <kalmatheroshan@gmail.com>
2024-07-06 10:41:58 +0530

    'change'

--- 10 Binary Tree/Questions/19.LCA.cpp
+++ 10 Binary Tree/Questions/19.LCA.cpp
@@ -88,12 +88,35 @@ void level_print(node *root)
       cout << endl;
    }
 }
+
+node *lca(node *root, int n1, int n2)
+{
+   if (root == 0)
+      return root;
+
+   if (n1 == root->val || n2 == root->val)
+      return root;
+
+   node *left = lca(root->left, n1, n2);
+
+   node *right = lca(root->right, n1, n2);
+
+   if (left == 0)
+      return right;
+   if (right == 0)
+      return left;
+      
+   return root;
+}
 int main()
 {
 
    node *root = 0;
    root = levelb(root);
    level_print(root);
+
+   cout << "Lca of 52 and 40 " << endl;
+   cout << lca(root, 52, 40)->val << endl;
 
    cout << "=== end ===" << endl;
 
